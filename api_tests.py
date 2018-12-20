9326ac4263161d66f6f9e4f501960eb03ac80201
realespee <wanyamasp@gmail.com>
2018-12-20 08:38:50 +0300

    write test for create red-flag endpoint

--- api_tests.py
+++ api_tests.py
@@ -9,3 +9,16 @@ def client():
         test_client = api.app.test_client()
     return test_client
 
+class TestApi():
+    def test_create_redflag(self, client):
+        red_flag = {
+            'id' : 1,
+            'title' : 'Bribery',
+            'latitude': 372823,
+            'longitude': 363837,
+            'Description': 'there is a need to learn this thing'
+        }
+        url = '/api/v1/redflags'
+        resp = client.post(url, data=json.dumps(red_flag), content_type='application/json')
+        assert red_flag in api.all_redflags
+        assert resp.json['status'] == 201
