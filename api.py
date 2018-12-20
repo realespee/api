9b58fb6bb7cf7ef0f0e6c5deaa0f915f569c421e
realespee <wanyamasp@gmail.com>
2018-12-20 08:23:23 +0300

    add create red-flag endpoint

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+all_redflags = []
 
+@app.route('/api/v1/redflags', methods=['POST'])
+def create_red_flag():
+    red_flag = request.json
+    all_redflags.append(red_flag)
+    return jsonify({"status" : 201, "data" : [{"id" : red_flag['id'], "message" : "Created red-flag record”"}]}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
