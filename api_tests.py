7a5c2018ef2dee5b300562ecee55611a5d015c6e
realespee <wanyamasp@gmail.com>
2018-12-20 08:37:19 +0300

    add test client

--- api_tests.py
+++ api_tests.py
@@ -1,4 +1,11 @@
 import pytest, api
 from flask import json
 
+@pytest.fixture
+def client():
+    '''define a test client'''
+    with api.app.app_context():
+        api.app.config['TESTING'] = True
+        test_client = api.app.test_client()
+    return test_client
 
