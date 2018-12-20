fc03694404f57829a9ff6e03af6656ce0eab03f2
realespee <wanyamasp@gmail.com>
2018-12-20 08:47:55 +0300

    create endpoint to GET all redflags

--- api.py
+++ api.py
@@ -10,5 +10,10 @@ def create_red_flag():
     all_redflags.append(red_flag)
     return jsonify({"status" : 201, "data" : [{"id" : red_flag['id'], "message" : "Created red-flag record”"}]}), 201
 
+@app.route('/api/v1/redflags', methods=['GET'])
+def get_red_flag():
+    return jsonify({"status" : 200, "data" : all_redflags})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
